511c404661b0d0043583cfe4ed2d4f19eec174aa
tasnimislam7 <annur.tasnim@grameendhs.com>
2026-07-13 10:49:44 +0600

    feat: string methods

---
+++ string_methods.py
@@ -0,0 +1,13 @@
+a = 'rahim'
+a = a.title()
+print(a)
+print(a.lower())
+print(a.swapcase())
+print(a)
+
+txt = 'I like bananas'
+x = txt.replace('bananas', 'mangoes')
+
+print(x)
+
+print(txt.count('a'))
