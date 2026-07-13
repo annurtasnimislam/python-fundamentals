ae073006c5be0a044bbc9e4cccd6dd7119608aa3
tasnimislam7 <annur.tasnim@grameendhs.com>
2026-07-13 10:50:08 +0600

    feat: basic python opertions

---
+++ operations.py
@@ -0,0 +1,14 @@
+a = 4+5
+b = 4-5
+c = 4*5
+d = 10/5  # float division
+e = 21/5
+f = 21//5  # integar division
+print(a, b, float(c), d, int(e), f)
+
+
+# BODMAS
+
+x = (2 + 4) - 5 + 18/2 + 12*2 - 5 ** 2
+#   = 6 -    -5   +9.0  + 24      -25
+print(x)
