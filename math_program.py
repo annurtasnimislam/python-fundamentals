092bee81eceb20783a3ec05edc425efd7431727e
tasnimislam7 <annur.tasnim@grameendhs.com>
2026-07-13 12:14:23 +0600

    feat: arithmetic operations

---
+++ math_program.py
@@ -0,0 +1,30 @@
+num1 = 50
+num2 = 10
+
+sum = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print(sum)
+print(sub)
+print(mul)
+print(div)
+
+# Numeric datatype : integer, float, complex
+num3 = 9
+num4 = 2
+
+div = num3 / num4
+float_division = num3 // num4
+
+print(div)
+print(float_division)
+
+rem = num3 % num4  # modulous operator
+
+print(rem)
+
+power = num3 ** num4
+
+print(power)
