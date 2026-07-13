6d92446ea41f5d6430a5fb72dda5c05bec243f93
tasnimislam7 <annur.tasnim@grameendhs.com>
2026-07-13 10:49:11 +0600

    feat: conditional statement

---
+++ conditional.py
@@ -0,0 +1,11 @@
+# Conditional State
+rain = 0
+# assignment operator ( = )
+10 == 10  # true
+10 == 4  # false
+# comparison operator ( == )
+
+if rain == 1:
+    print('successful')
+else:
+    print('failed')
